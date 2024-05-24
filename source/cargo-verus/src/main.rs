400a51078459fa6b88a82aa286d1244600859c3e
Nick Spinale <nick@nickspinale.com>
2024-05-24 01:05:07 +0000

    x

--- source/cargo-verus/src/main.rs
+++ source/cargo-verus/src/main.rs
@@ -118,6 +118,7 @@ impl VerusCmd {
         let mut cmd = MetadataCommand::new();
         cmd.other_options(cargo_metadata_args);
         cmd.exec().unwrap_or_else(|err| {
+            // TODO
             panic!("cargo metadata command {:?} failed: {:?}", cmd, err);
         })
     }
@@ -152,7 +153,12 @@ impl VerusCmd {
             let package_id =
                 mk_package_id(&package.name, package.version.to_string(), &package.manifest_path);
 
-            let verus_metadata = get_verus_metadata(package);
+            let mut verus_metadata = get_verus_metadata(package);
+
+            // The verify, is_builtin, and is_builtin_macro fields are passed as env vars as they
+            // are relevant for crates which are skipped by Verus. In such cases, the driver avoids
+            // depending on __VERUS_DRIVER_ARGS__ to prevent unecessary rebuilds when its value
+            // changes.
 
             if verus_metadata.verify {
                 cmd.env(format!("__VERUS_DRIVER_VERIFY_{package_id}"), "1");
@@ -178,6 +184,15 @@ impl VerusCmd {
 
             if verus_metadata.is_core {
                 verus_driver_args_for_package.push("--verus-arg=--is-core".to_owned());
+            }
+
+            if !verus_metadata.no_vstd {
+                let key = "vstd".to_owned();
+                if verus_metadata.imports.contains(&key) {
+                    // TODO
+                    panic!("package.metadata.verus.no-vstd implies package.metadata.verus.imports = [\"vstd\"]");
+                }
+                verus_metadata.imports.push(key);
             }
 
             for import in verus_metadata.imports.iter() {
@@ -227,6 +242,7 @@ fn filter_args(
                 orig_args
                     .next()
                     .unwrap_or_else(|| {
+                        // TODO
                         panic!("expected {} to be followed by a value", arg.as_ref())
                     })
                     .as_ref()
@@ -273,6 +289,7 @@ fn get_verus_metadata(package: &cargo_metadata::Package) -> VerusMetadata {
         .and_then(|obj| obj.get("verus"))
         .map(|v| {
             serde_json::from_value::<VerusMetadata>(v.clone()).unwrap_or_else(|err| {
+                // TODO
                 panic!(
                     "failed to parse {}-{}.metadata.verus: {}",
                     package.name, package.version, err
